b84bf6fa71449b04dccaea715ce5473d5a44d016
ALIRAZA705 <aliraza30028@gmail.com>
2023-05-24 10:44:29 +0500

    ui updated for add , edit delete

--- src/pages/Task/DisplayTaskList.js
+++ src/pages/Task/DisplayTaskList.js
@@ -9,192 +9,230 @@ import Paper from "@mui/material/Paper";
 import DeleteIcon from "../../assets/svgs/DeleteIcon";
 import EditIcon from "../../assets/svgs/EditIcon";
 import Button from "../../components/Button/Button";
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import NoDataImage from "../../assets/pngs/rainy-day.png";
 
 function createData(title, description, completionStatus) {
-  return { title, description, completionStatus };
+    return { title, description, completionStatus };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
 ];
 
 export default function DisplayTaskList(props) {
-  const { taskList, handleRemoveTask, handleEditTask } = props;
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography
-                alignSelf="center"
-                sx={{
-                  // marginTop: "20px",
-                  fontFamily: "Poppins-Medium",
-                  fontSize: "14px",
-                  fontWeight: 400,
-                  lineHeight: "24px",
-                  color: "#292A3D",
-                  letterSpacing: "0.15px",
-                }}
-              >
-                Title
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography
-                alignSelf="center"
-                sx={{
-                  // marginTop: "20px",
-                  fontFamily: "Poppins-Medium",
-                  fontSize: "14px",
-                  fontWeight: 400,
-                  lineHeight: "24px",
-                  color: "#292A3D",
-                  letterSpacing: "0.15px",
-                }}
-              >
-                Description
-              </Typography>
-            </TableCell>
-            <TableCell align="right">
-              <Typography
-                alignSelf="center"
-                sx={{
-                  // marginTop: "20px",
-                  fontFamily: "Poppins-Medium",
-                  fontSize: "14px",
-                  fontWeight: 400,
-                  lineHeight: "24px",
-                  color: "#292A3D",
-                  letterSpacing: "0.15px",
-                }}
-              >
-                Completion Status
-              </Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography
-                alignSelf="center"
-                sx={{
-                  // marginTop: "20px",
-                  fontFamily: "Poppins-Medium",
-                  fontSize: "14px",
-                  fontWeight: 400,
-                  lineHeight: "24px",
-                  color: "#292A3D",
-                  letterSpacing: "0.15px",
-                }}
-              >
-                Actions
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {taskList.map((task) => (
-            <TableRow
-              key={task.taskId}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <Typography
-                  alignSelf="center"
-                  sx={{
-                    fontFamily: "Poppins-Medium",
-                    fontSize: "12px",
-                    fontWeight: 400,
-                    lineHeight: "24px",
-                    color: "#292A3D",
-                    letterSpacing: "0.15px",
-                  }}
-                >
-                  {task.title}
-                </Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography
-                  alignSelf="center"
-                  sx={{
-                    fontFamily: "Poppins-Medium",
-                    fontSize: "12px",
-                    fontWeight: 400,
-                    lineHeight: "24px",
-                    color: "#292A3D",
-                    letterSpacing: "0.15px",
-                  }}
-                >
-                  {task.description}
-                </Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography
-                  alignSelf="center"
-                  sx={{
-                    fontFamily: "Poppins-Medium",
-                    fontSize: "12px",
-                    fontWeight: 400,
-                    lineHeight: "24px",
-                    color: "#292A3D",
-                    letterSpacing: "0.15px",
-                  }}
-                >
-                  {task.completionStatus.key}
-                </Typography>
-              </TableCell>
-              <TableCell align="center">
-                <Stack
-                  direction="row"
-                  spacing={2}
-                  display="flex"
-                  justifyContent="flex-end"
-                >
-                  <Button
-                    type="button"
-                    variant="primary"
-                    style={{
-                      color: "#131599",
-                      backgroundColor: "#F1EFFF",
-                      border: "none",
-                    }}
-                    onClick={() => {
-                      handleEditTask(task);
-                    }}
-                    size="sm"
-                    leadIcon={
-                      <EditIcon
-                        color="#131599"
-                        style={{ width: "16px", height: "16px" }}
-                      />
-                    }
-                  >
-                    Edit Profile
-                  </Button>
+    const { taskList, handleRemoveTask, handleEditTask } = props;
+    return (
+        <TableContainer
+            component={Paper}
+            sx={{
+                minWidth: "auto",
+                mt: 2,
+                background: "none",
+                borderRadius: "18px !important",
+                border: "3px solid grey",
+                // boxShadow: "0 5px 10px 0 rgba(0, 0, 0, 0.1)",
+                boxShadow: "none",
+            }}
+        >
+            {taskList.length > 0 ? (
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                <Typography
+                                    alignSelf="center"
+                                    sx={{
+                                        // marginTop: "20px",
+                                        fontFamily: "Poppins-Bold",
+                                        fontSize: "13px",
+                                        fontWeight: 400,
+                                        lineHeight: "24px",
+                                        color: "#292A3D",
+                                        letterSpacing: "-0.5px",
+                                    }}
+                                >
+                                    Title
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Typography
+                                    alignSelf="center"
+                                    sx={{
+                                        // marginTop: "20px",
+                                        fontFamily: "Poppins-Bold",
+                                        fontSize: "13px",
+                                        fontWeight: 400,
+                                        lineHeight: "24px",
+                                        color: "#292A3D",
+                                        letterSpacing: "-0.5px",
+                                    }}
+                                >
+                                    Description
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                                <Typography
+                                    alignSelf="center"
+                                    sx={{
+                                        // marginTop: "20px",
+                                        fontFamily: "Poppins-Bold",
+                                        fontSize: "13px",
+                                        fontWeight: 400,
+                                        lineHeight: "24px",
+                                        color: "#292A3D",
+                                        letterSpacing: "-0.5px",
+                                    }}
+                                >
+                                    Completion Status
+                                </Typography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <Typography
+                                    alignSelf="center"
+                                    sx={{
+                                        // marginTop: "20px",
+                                        fontFamily: "Poppins-Bold",
+                                        fontSize: "13px",
+                                        fontWeight: 400,
+                                        lineHeight: "24px",
+                                        color: "#292A3D",
+                                        letterSpacing: "-0.5px",
+                                    }}
+                                >
+                                    Actions
+                                </Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {taskList.map((task) => (
+                            <TableRow
+                                key={task.taskId}
+                                sx={{
+                                    "&:last-child td, &:last-child th": {
+                                        border: 0,
+                                    },
+                                }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    <Typography
+                                        alignSelf="center"
+                                        sx={{
+                                            fontFamily: "Poppins-Medium",
+                                            fontSize: "12px",
+                                            fontWeight: 400,
+                                            lineHeight: "24px",
+                                            color: "#292A3D",
+                                            letterSpacing: "0.15px",
+                                        }}
+                                    >
+                                        {task.title}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align="right">
+                                    <Typography
+                                        alignSelf="center"
+                                        sx={{
+                                            fontFamily: "Poppins-Medium",
+                                            fontSize: "12px",
+                                            fontWeight: 400,
+                                            lineHeight: "24px",
+                                            color: "#292A3D",
+                                            letterSpacing: "0.15px",
+                                        }}
+                                    >
+                                        {task.description}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align="right">
+                                    <Typography
+                                        alignSelf="center"
+                                        sx={{
+                                            fontFamily: "Poppins-Medium",
+                                            fontSize: "12px",
+                                            fontWeight: 400,
+                                            lineHeight: "24px",
+                                            color: "#292A3D",
+                                            letterSpacing: "0.15px",
+                                        }}
+                                    >
+                                        {task.completionStatus.key}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell align="center">
+                                    <Stack
+                                        direction="row"
+                                        spacing={2}
+                                        display="flex"
+                                        justifyContent="flex-end"
+                                    >
+                                        <Button
+                                            type="button"
+                                            variant="outlined"
+                                            style={{
+                                                color: "#264FB0",
+                                                // backgroundColor: "#F1EFFF",
+                                                border: "2px solid #26B086",
+                                            }}
+                                            onClick={() => {
+                                                handleEditTask(task);
+                                            }}
+                                            size="xs"
+                                            leadIcon={
+                                                <EditIcon
+                                                    color="#fff"
+                                                    style={{
+                                                        width: "16px",
+                                                        height: "16px",
+                                                    }}
+                                                />
+                                            }
+                                        ></Button>
 
-                  <Button
-                    type="button"
-                    variant="outlined"
-                    size="sm"
-                    style={{
-                      color: "#BA1A1A",
-                      fontSize: "14px",
-                      fontFamily: "Poppins-Medium",
-                      lineHeight: "20px",
-                    }}
-                    onClick={() => handleRemoveTask(task)}
-                    leadIcon={<DeleteIcon />}
-                  >
-                    Remove User
-                  </Button>
-                </Stack>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                            size="xs"
+                                            style={{
+                                                color: "#fff",
+                                            }}
+                                            onClick={() =>
+                                                handleRemoveTask(task)
+                                            }
+                                            leadIcon={<DeleteIcon />}
+                                        >
+                                            Remove
+                                        </Button>
+                                    </Stack>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            ) : (
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    rowGap={2}
+                    margin="auto"
+                    justifyContent="center"
+                    alignItems="center"
+                    my={4}
+                >
+                    <img
+                        src={NoDataImage}
+                        alt="No Data"
+                        height="auto"
+                        width="160vh"
+                    />
+                    <Typography variant="subtitle2">
+                        No tasks here boss
+                    </Typography>
+                </Box>
+            )}
+        </TableContainer>
+    );
 }
