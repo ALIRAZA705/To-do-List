e48227ce3b936910049db7b5ad9b43729c3cecde
ALIRAZA705 <aliraza30028@gmail.com>
2023-05-24 11:02:11 +0500

    ui fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Task from "./pages/Task";
+import Logo from "./assets/svgs/Logo";
+import { Container, Typography } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <Task />
-    </div>
-  );
+    return (
+        <Container className="Container">
+            <Logo className="logo" />
+            <Typography
+                alignSelf="center"
+                sx={{
+                    // marginTop: "20px",
+                    fontFamily: "Poppins-Black",
+                    fontSize: 26,
+                    fontWeight: 900,
+                    lineHeight: "24px",
+                    pl: "32px",
+                    color: "#F25F38",
+                    letterSpacing: "-1px",
+                }}
+            >
+                TODO
+            </Typography>
+            <Task />
+        </Container>
+    );
 }
 
 export default App;
-
